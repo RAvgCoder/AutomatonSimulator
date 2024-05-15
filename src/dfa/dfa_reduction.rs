614c1e9059501e9bb8afdff52de03f8e3e130ef2
egbor <egborosebhulimen8@gmail.com>
2024-05-15 00:07:46 -0300

    Added documentation, comments Changed the validation for checking if an automaton can be reduced. Changed how creation of final and non-final equivalence classes done.

--- src/dfa/dfa_reduction.rs
+++ src/dfa/dfa_reduction.rs
@@ -1,12 +1,12 @@
-mod dfa_step_renderer;
-mod equivalence_class;
-
 use std::collections::{HashMap, HashSet};
 
 use crate::automaton_graph::Automaton;
-use crate::dfa::DFA;
 use crate::dfa::dfa_reduction::dfa_step_renderer::DFAReductionStepsRenderer;
 use crate::dfa::dfa_reduction::equivalence_class::EquivalenceClass;
+use crate::dfa::{ReductionSteps, DFA};
+
+mod dfa_step_renderer;
+mod equivalence_class;
 
 impl DFA {
     /// Steps used by the dfa reduce function
@@ -22,31 +22,7 @@ impl DFA {
     ///----         Then create new equivalence classes which are the subdivisions of the current equivalence class
     ///
     /// Pick another state
-    pub fn reduce(&self) {
-        if !self.check_if_can_reduce() {
-            return;
-        }
-
-        let class_state_id = "s1";
-        let connecting_points = "C1 C1";
-        let padding_size = 5;
-
-        let formatted = format!(
-            "|{: <padding$}| {}",
-            class_state_id,
-            connecting_points,
-            padding = padding_size
-        );
-        println!("{}", formatted);
-        let class_state_id = "start";
-        let formatted = format!(
-            "|{: <padding$}| {}",
-            class_state_id,
-            connecting_points,
-            padding = padding_size
-        );
-        println!("{}", formatted);
-
+    pub fn reduce(&self) -> Option<ReductionSteps> {
         // Map of sate name to transition names
         let state_map: HashMap<String, Vec<String>> =
             Automaton::get_state_map(&self.automaton_graph);
@@ -54,41 +30,55 @@ impl DFA {
         let mut equiv_class_list: Vec<EquivalenceClass> =
             Self::get_final_and_non_final_states(&self.automaton_graph);
 
-        let mut reduction_steps = DFAReductionStepsRenderer::new(
+        // Check if a reduction is possible
+        if !self.check_if_can_reduce(&equiv_class_list) {
+            return None;
+        }
+
+        // Create a step renderer
+        let mut step_renderer: DFAReductionStepsRenderer = DFAReductionStepsRenderer::new(
             &state_map,
             &equiv_class_list,
-            self.automaton_graph.all_states(),
+            self.automaton_graph
+                .all_states()
+                .first()
+                .expect("Cannot reduce dfa with no states"),
         );
 
+        // Reduce the dfa
         let mut idx = 0;
         while idx < equiv_class_list.len() {
-            let equiv_class = equiv_class_list.get(idx).unwrap();
+            // Class picked for subdivision
+            let equiv_class: &EquivalenceClass = equiv_class_list.get(idx).unwrap();
 
             // <ClassNameConcat,Vec<State_belonging_to_the_class>>
             let mut sub_divisions: HashMap<String, Vec<String>> = HashMap::new();
 
-            // Get name of the state dividing
-            reduction_steps.add_dividing_name(equiv_class);
+            // Add the name of the state were are to possibly divide
+            step_renderer.add_name_of_sub_dividing_class(equiv_class);
 
+            // Check if state should be subdivided
             for state_id in equiv_class.state_ids() {
                 // Ex: [C0, C3, C4]
                 let class_transition_names =
                     Self::find_class_for_transitions(&state_map, &equiv_class_list, state_id);
 
-                reduction_steps.add_divisions(state_id, &class_transition_names);
+                step_renderer.track_sub_divisions(state_id, &class_transition_names);
 
-                // Add to map
+                // Collect all class_transitions to a map so see if they are all unique
+                // This also acts like a group by function grouping by class_transition names
+                // mapping to list of states which have similar transition names
                 sub_divisions
-                    .entry(class_transition_names.join("")) // Use the class_name name as the hashmap key
+                    .entry(class_transition_names) // Use the class_name name as the hashmap key
                     .or_insert_with(Vec::new) // If the entry doesn't exist, insert a new vector
                     .push(state_id.clone()); // Add the class_state_id to the vector
             }
 
-            // It needs to be broken up
+            // Class needs to be broken up as not all states map to the same
+            // equivalent class
             if sub_divisions.len() > 1 {
-
-                // Create new subdivided classes
-                let mut new_split_classes = sub_divisions
+                // Creates new subdivided classes
+                let mut new_divided_classes = sub_divisions
                     .values()
                     .map(|division| {
                         EquivalenceClass::new(
@@ -107,13 +97,11 @@ impl DFA {
                 let class_len = equiv_class_list.len();
 
                 // Add subdivided classes
-                equiv_class_list.append(&mut new_split_classes);
+                equiv_class_list.append(&mut new_divided_classes);
 
-                // Requires that the overloaded class be removed from the list before adding new ones subdivided classes
-                reduction_steps.add_split_classes(
-                    &equiv_class_list[class_len..],
-                    &equiv_class_list,
-                );
+                // Requires that the class that's to be divided be removed from the list before
+                // adding the new ones subdivided classes
+                step_renderer.add_split_classes(&equiv_class_list[class_len..], &equiv_class_list);
 
                 // Rebuild all equivalence classes
                 idx = 0;
@@ -121,36 +109,55 @@ impl DFA {
                 idx += 1;
             }
 
-            reduction_steps.split_conclusion();
+            // Add remarks on the state of the subdivisions
+            step_renderer.split_conclusion();
         }
 
-        dbg!(&equiv_class_list);
-        println!("{}", reduction_steps);
+        // Print out the steps taken and the tables generated
+        println!("{}", step_renderer);
+
+        // TODO Create and return an automaton representing the reduced dfa
+        Some(ReductionSteps {
+            table: step_renderer.move_table_steps(),
+            steps: step_renderer.move_steps(),
+        })
     }
 
-    fn check_if_can_reduce(&self) -> bool {
+    /// Acts like an assert to check if the automaton qualifies for a reduction
+    fn check_if_can_reduce(&self, equiv_class_list: &Vec<EquivalenceClass>) -> bool {
         return if self.automaton_graph.all_states().is_empty() {
-            println!("Cannot reduce the dfa as there are no states to reduce");
+            eprintln!("Cannot reduce the dfa as there are no states to reduce");
             false
         } else if self.automaton_graph.all_states()[0]
             .get_transitions()
             .is_empty()
         {
-            println!("There are no transitions on this graph so it can not be reduced");
+            eprintln!("There are no transitions on this graph so it can not be reduced");
+            false
+        } else if equiv_class_list.len() != 2 {
+            eprintln!(
+                "It only contains either final / non-final sates hence no need for a reduction"
+            );
             false
         } else {
             true
         };
     }
 
-
-    // Ex: [C0, C3, C4]
-    /// WARNING: `equiv_states_list` must contain a list of all equivalence classes
+    /// Returns a string of classes corresponding to the transitions on a `class_state_id`
+    ///
+    /// Ex: [C0, C3, C4]
+    ///
+    /// # Arguments
+    ///
+    /// * `state_map`: A map of all states and a list of states they transition to
+    /// * `complete_equiv_states_list`: This `must` contain a list of all equivalence classes
+    /// * `class_state_id`: The state you would like to find the equivalent class for each transitions
     fn find_class_for_transitions(
         state_map: &HashMap<String, Vec<String>>,
-        equiv_states_list: &Vec<EquivalenceClass>,
+        complete_equiv_states_list: &Vec<EquivalenceClass>,
         class_state_id: &String,
-    ) -> Vec<String> {
+    ) -> String {
         state_map
             .get(class_state_id)
             .expect(&format!(
@@ -158,12 +165,17 @@ impl DFA {
                 class_state_id
             ))
             .iter()
-            .map(|state_name| Self::find_equiv_class_name(state_name, equiv_states_list))
+            .map(|state_name| Self::find_equiv_class_name(state_name, complete_equiv_states_list))
             .collect::<Vec<String>>()
+            .join(" ")
     }
 
-    /// returns a list of final and non_final equivalence_classes
+    /// Returns a list of final and non_final equivalence_classes
     /// respectively if they exist
+    ///
+    /// # Arguments
+    ///
+    /// * `automaton`: The automaton to reduce
     fn get_final_and_non_final_states(automaton: &Automaton) -> Vec<EquivalenceClass> {
         let mut final_states = HashSet::new();
         let mut non_final_states = HashSet::new();
@@ -178,25 +190,23 @@ impl DFA {
 
         let mut equiv_classes: Vec<EquivalenceClass> = vec![];
 
-        // Order matters
-        let final_state_index = 0;
-        let mut non_final_state_index = 1;
-        let mut splits = vec![
-            EquivalenceClass::new(final_states),
-            EquivalenceClass::new(non_final_states),
-        ];
-
-        if !splits[final_state_index].is_empty() {
-            equiv_classes.push(splits.remove(final_state_index));
-            non_final_state_index = 0;
+        if !final_states.is_empty() {
+            equiv_classes.push(EquivalenceClass::new(final_states));
         }
 
-        if !splits[non_final_state_index].is_empty() {
-            equiv_classes.push(splits.remove(non_final_state_index))
+        if !non_final_states.is_empty() {
+            equiv_classes.push(EquivalenceClass::new(non_final_states))
         }
 
         equiv_classes
     }
+
+    /// Given a state name it finds the corresponding equivalence class it corresponds to in the list provided
+    ///
+    /// # Arguments
+    ///
+    /// * `state_name`: The state you want find the class name for
+    /// * `equiv_states_list`: List of equivalent states you want to search though
     fn find_equiv_class_name(
         state_name: &String,
         equiv_states_list: &Vec<EquivalenceClass>,
@@ -208,7 +218,7 @@ impl DFA {
                 "Could not find {} in any of the equivalence classes",
                 state_name
             ))
-            .class_name()
+            .prefix_name()
             .clone()
     }
 }
