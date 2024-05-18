8945431cd518e74e1ce224e18043a7c217f8c87e
egbor <egborosebhulimen8@gmail.com>
2024-05-18 00:57:19 -0300

    feat: Added a way to relay the results after dfa reduction

--- src/dfa.rs
+++ src/dfa.rs
@@ -6,13 +6,27 @@ pub struct DFA {
     automaton_graph: Automaton,
 }
 
+#[derive(Debug)]
 pub struct ReductionSteps {
-    table: Vec<String>,
-    steps: Vec<String>,
+    classes_created: u32,
+    pub steps: Vec<String>,
+    pub table: Vec<String>,
+    pub reduced_dfa: Automaton,
+}
+
+impl ReductionSteps {
+    fn classes_created(&self) -> u32 {
+        self.classes_created
+    }
 }
 
 impl DFA {
     pub fn new(automaton: Automaton) -> Self {
+        // Makes all transition have the same order of symbols
+        for s in automaton.all_states() {
+            s.transition_edges.borrow_mut().sort()
+        }
+
         DFA {
             automaton_graph: automaton,
         }
