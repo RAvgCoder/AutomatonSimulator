a483275e0eab84b9387ec805b60ab93f68bc69d9
egbor <egborosebhulimen8@gmail.com>
2024-05-31 00:18:25 -0300

    feat: Impl from for symbols

--- src/automaton_graph/symbol.rs
+++ src/automaton_graph/symbol.rs
@@ -1,12 +1,19 @@
-use crate::automaton_graph::Symbol;
 use std::fmt;
 use std::fmt::{Display, Formatter};
 
+use crate::automaton_graph::Symbol;
+
 impl Display for Symbol {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match &self {
-            Symbol::CHAR(c) => write!(f, "{}", c),
-            Symbol::EPSILON => write!(f, "ϵ"),
+        write!(f, "{}", char::from(self.clone()))
+    }
+}
+
+impl From<Symbol> for char {
+    fn from(value: Symbol) -> char {
+        match value {
+            Symbol::CHAR(c) => c,
+            Symbol::EPSILON => 'ϵ',
         }
     }
 }
