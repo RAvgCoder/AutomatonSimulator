3b97bebfc0dbab0b4b782dc96046a69dbaa23dc0
Osebhulimen Egbor <os614875@dal.ca>
2024-06-07 23:02:49 -0300

    feat: Implemented dfa simulation, and moved simulation symbols to another file

--- src/dfa/dfa_simulator.rs
+++ src/dfa/dfa_simulator.rs
@@ -1,31 +1,24 @@
-use std::fmt;
-use std::fmt::{Debug, Formatter};
+use std::fmt::Debug;
 
-use crate::automaton_graph::{Symbol, Transition};
+use crate::automaton_graph::{SimulationError, Symbol, Transition};
 use crate::dfa::DFA;
 
-pub enum SimulationError {
-    NonTransitionSymbols(String),
-}
-
-impl Debug for SimulationError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            SimulationError::NonTransitionSymbols(msg) => {
-                write!(f, "Non-transition symbols error: {}", msg)
-            }
-        }
-    }
-}
-
 impl DFA {
-    pub fn simulate(&self, simulating_string: &String) -> Result<bool, SimulationError> {
+    pub fn simulate(&self, simulating_string: &str) -> Result<bool, SimulationError> {
         let mut curr_state = self.automaton_graph.get_start_state();
 
         for (idx, c) in simulating_string.chars().enumerate() {
-            let transition = Transition::find_transition_by_symbol(curr_state.get_transitions(), c)
-                .ok_or_else(|| SimulationError::NonTransitionSymbols(format!(
-                    "Invalid character {} at index {} for symbols {:?} on state {}",
+            let transition = Transition::find_transition_by_symbol(curr_state.get_transitions(), c);
+
+            if transition.len() != 1 {
+                // DFAs must only have one valid transition
+                return Err(SimulationError::NoTransitionForSymbol(format!(
+                    "{} found for character {} at index {} for symbols {:?} on state {}",
+                    if transition.len() == 0 {
+                        "No transition"
+                    } else {
+                        "Multiple transitions"
+                    },
                     c,
                     idx,
                     curr_state
@@ -35,9 +28,10 @@ impl DFA {
                         .map(|t| t.transition_on())
                         .collect::<Vec<Symbol>>(),
                     &curr_state.alt_id
-                )))?;
+                )));
+            }
 
-            curr_state = transition.to();
+            curr_state = transition.first().unwrap().to();
         }
 
         Ok(curr_state.is_accept_state)
